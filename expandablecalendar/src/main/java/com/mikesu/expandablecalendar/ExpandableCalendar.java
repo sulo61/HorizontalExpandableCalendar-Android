cce5aefe3b7bea46fd2679aafc486158e5e63817
Michał Sułek <michal.sulek@miquido.com>
2016-08-04 15:21:49 +0200

    add basic dependency to activity

--- expandablecalendar/src/main/java/com/mikesu/expandablecalendar/ExpandableCalendar.java
+++ expandablecalendar/src/main/java/com/mikesu/expandablecalendar/ExpandableCalendar.java
@@ -3,13 +3,14 @@ package com.mikesu.expandablecalendar;
 import android.content.Context;
 import android.util.AttributeSet;
 import android.view.View;
+import android.widget.RelativeLayout;
 
 /**
  * Created by MikeSu on 04/08/16.
  * www.michalsulek.pl
  */
 
-public class ExpandableCalendar extends View {
+public class ExpandableCalendar extends RelativeLayout {
 
   public ExpandableCalendar(Context context) {
     super(context);
